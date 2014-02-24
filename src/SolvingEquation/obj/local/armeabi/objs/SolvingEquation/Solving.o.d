50d8ff599acea5305e935b487b47cc2832f62a93
alexcpp2013 <alex.melnik@iua>
2014-02-24 19:09:37 +0200

    first commit

---
+++ src/SolvingEquation/obj/local/armeabi/objs/SolvingEquation/Solving.o.d
@@ -0,0 +1,162 @@
+obj/local/armeabi/objs/SolvingEquation/Solving.o: jni/Solving.cpp \
+ jni/Solving.h jni/def.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/android/log.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/stdarg.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/stl/_cprolog.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/stl/config/features.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/stl/_stlport_version.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/stl/config/user_config.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/stl/config/compat.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/stl/config/host.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/stl/config/_system.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/stl/config/_android.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/sys/cdefs.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/android/api-level.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/stl/config/_gcc.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/stl/config/stl_confix.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/stl/config/_native_headers.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/stl/_epilog.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/stl/_config_compat_post.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/string.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/../include/string.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/stddef.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/malloc.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/stdio.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/../include/stdio.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/sys/types.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/stdint.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/sys/_types.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/machine/_types.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/linux/posix_types.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/linux/stddef.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/linux/compiler.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/asm/posix_types.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/asm/types.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/linux/types.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/machine/kernel.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/stdlib.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/../include/stdlib.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/alloca.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/strings.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/memory.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/math.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/../include/math.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/limits.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/../include/limits.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/sys/limits.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/linux/limits.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/machine/internal_types.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/machine/limits.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/sys/syslimits.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/asm/page.h \
+ C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/jni.h
+
+jni/Solving.h:
+
+jni/def.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/android/log.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/stdarg.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/stl/_cprolog.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/stl/config/features.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/stl/_stlport_version.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/stl/config/user_config.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/stl/config/compat.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/stl/config/host.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/stl/config/_system.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/stl/config/_android.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/sys/cdefs.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/android/api-level.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/stl/config/_gcc.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/stl/config/stl_confix.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/stl/config/_native_headers.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/stl/_epilog.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/stl/_config_compat_post.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/string.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/../include/string.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/stddef.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/malloc.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/stdio.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/../include/stdio.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/sys/types.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/stdint.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/sys/_types.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/machine/_types.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/linux/posix_types.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/linux/stddef.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/linux/compiler.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/asm/posix_types.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/asm/types.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/linux/types.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/machine/kernel.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/stdlib.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/../include/stdlib.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/alloca.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/strings.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/memory.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/math.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/../include/math.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/sources/cxx-stl/stlport/stlport/limits.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/../include/limits.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/sys/limits.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/linux/limits.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/machine/internal_types.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/machine/limits.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/sys/syslimits.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/asm/page.h:
+
+C:/Dev/android/android-ndk-r9c-windows-x86/android-ndk-r9c/platforms/android-14/arch-arm/usr/include/jni.h:
